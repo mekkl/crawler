8a9b4cec53b0d654cee6cef8b89cfe1c490ce10d
ML <mikkel7emil@gmail.com>
2018-10-28 14:26:19 +0100

    working projecct

---
+++ crawler.py
@@ -0,0 +1,164 @@
+
+import bs4
+import sys
+import os
+import requests
+import re
+import pprint
+from selenium import webdriver
+from tqdm import tqdm
+import json
+from dateparser import parse
+import argparse # https://docs.python.org/3/howto/argparse.html
+
+
+def scrape_no_events(url):
+    """
+    Find amount of pages to parse from the entry html page
+    
+    returns:
+        An integer with the amount of events
+    """
+    r = requests.get(url)
+    r.raise_for_status()
+    soup = bs4.BeautifulSoup(r.text, 'html5lib')
+
+    tables = soup.select('table')
+
+    # The second table of depth three holds the amount of events
+    depth3 = [t for t in tables if len(t.find_parents('table')) == 3]
+    # The only bold element holds the amount of events
+    event_str = depth3[1].select('b')[0].text.splitlines()[0]
+
+    reg_exp = r'Showing .+ of (?P<events>\d+)'
+    m = re.search(reg_exp, str(event_str))
+    no_events = int(m.group('events'))
+    
+    return no_events
+
+def scrape_events_per_page(url):
+    """
+    returns:
+        A list of tuples of strings holding title, place, date, and price
+        for concerts in Copenhagen scraped from Kulturnaut.dk
+    """
+    r = requests.get(url)
+    r.raise_for_status()
+
+    soup = bs4.BeautifulSoup(r.text, 'html5lib')
+    event_cells = soup.find_all('td', {'width': '100%', 'valign' : 'top'})
+    scraped_events_per_page = []
+    for event_cell in event_cells:
+        try:
+            title = event_cell.find('b').text
+            spans = event_cell.find_all('span')
+            place = spans[1].text
+            try:
+                date, price = spans[0].text.splitlines()
+            except ValueError as e:
+                date = spans[0].text.splitlines()[0]
+                price = ''
+        except Exception as e:
+            print(e)
+            
+        scraped_events_per_page.append((title, place, date, price))
+        
+    return scraped_events_per_page
+
+
+def get_dates_and_prices(scraped_events):
+    """
+    Cleanup the data. Get price as integer and date as date.
+    
+    returns:
+        A two-element tuple with a datetime representing the start 
+        time of an event and an integer representing the price in Dkk.
+    """
+
+    price_regexp = r"(?P<price>\d+)"
+
+    data_points = []
+
+    for event_data in tqdm(scraped_events):
+        title_str, place_str, date_str, price_str = event_data
+        
+        if 'Free admission' in price_str:
+            price = 0
+        else:
+            m = re.search(price_regexp, price_str)
+            try:
+                price = int(m.group('price'))
+            except:
+                price = 0
+
+        date_str = date_str.strip().strip('.')
+        if '&' in date_str:
+            date_str = date_str.split('&')[0]
+        if '-' in date_str:
+            date_str = date_str.split('-')[0]
+        if '.' in date_str:
+            date_str = date_str.replace('.', ':')
+        
+        date = parse(date_str)
+        if date:
+            data_points.append((date, price))
+            
+    return data_points
+
+def scrape_links(from_url, for_depth=0, all_links={}):
+    '''
+        URL validation: https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
+    '''
+
+    links = all_links.copy()
+    regex = re.compile( r'^(?:http|ftp)s?://', re.IGNORECASE) # http:// or https://
+
+    # -- Continue if depth >= 0 and if link doesn't already have been scraped -- 
+    if for_depth >= 0 and links.get(from_url) is None: 
+        try:
+            r = requests.get(from_url)
+            r.raise_for_status() # raise hvis status for get ikke er ok
+            soup = bs4.BeautifulSoup(r.text, 'html5lib')
+            a_tags = soup.find_all('a') # find all a tags
+
+            for a_tag in tqdm(a_tags):
+                url = a_tag.attrs.get('href', '')
+                if re.match(regex, url) is not None: # If regex matches (is a URL)
+                    if links.get(from_url) is None: # Create empty list for from_url (key)
+                        links.setdefault(from_url, []).append(url)
+                    else: # add to list for from_url (key)
+                        links[from_url].append(url)
+
+                    links = scrape_links(url, for_depth - 1, links) # RECURSIVE STEP
+        except:
+            return links # return 
+
+    return links
+
+
+if __name__ == '__main__':
+    print('')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("url", help="Starting point for crawler")
+    parser.add_argument('-j', '--json', help='JSON dump')
+    parser.add_argument("-d", "--depth", help="Crawler Depth", type=int)
+    args = parser.parse_args()
+
+    url = args.url
+    file_name = args.json if args.json else None
+    depth = args.depth if args.depth else 0
+
+    scraped_links = scrape_links(url, depth)
+
+    if file_name is not None:
+        with open(f'{file_name}.json', 'w') as fp:
+            json.dump(scraped_links, fp)
+    else:
+        print(scraped_links)
+
+    if depth > 0:
+        print('\n') 
+    else:
+        print('')
+
