cf145110a2451f2418fa843485ee4bc69db22e3a
ML <mikkel7emil@gmail.com>
2018-10-29 01:24:45 +0100

    create console logging + timer functionality

--- crawler.py
+++ crawler.py
@@ -4,6 +4,8 @@ import sys
 import os
 import requests
 import re
+import time
+import datetime
 import pprint
 from selenium import webdriver
 from tqdm import tqdm
@@ -105,60 +107,99 @@ def get_dates_and_prices(scraped_events):
             
     return data_points
 
-def scrape_links(from_url, for_depth=0, all_links={}):
+## REPRESENT CLASS ATTRIBUTE start and epoch params!!!!  !!! 
+## TODO: Change scrape_links() to class with attribute!!!!
+def scrape_links(from_url, for_depth=0, all_links={'found': 0, 'links':{}}, start=None, epoch=None):
     '''
         URL validation: https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
+        HTML validation: http://www.mkyong.com/regular-expressions/how-to-validate-html-tag-with-regular-expression/
     '''
+    if not bool(all_links['links']):
+        start = datetime.datetime.now()
 
     links = all_links.copy()
-    regex = re.compile( r'^(?:http|ftp)s?://', re.IGNORECASE) # http:// or https://
+    regex_url = re.compile( r'^(?:http|ftp)s?://', # http:// or https://
+                        re.IGNORECASE) 
+    regex_html = re.compile(r"<(\"[^\"]*\"|'[^']*'|[^'\">])*>", re.IGNORECASE) # HTML elements
 
-    # -- Continue if depth >= 0 and if link doesn't already have been scraped -- 
-    if for_depth >= 0 and links.get(from_url) is None: 
-        try:
-            r = requests.get(from_url)
+    # ---- Continue if depth >= 0 and if link doesn't already have been scraped ----
+    if for_depth >= 0 and links['links'].get(from_url) is None: 
+        
+        try: # ---- try, except block if requests raises exception ----
+            r = requests.get(from_url, timeout=(2,2))
             r.raise_for_status() # raise hvis status for get ikke er ok
-            soup = bs4.BeautifulSoup(r.text, 'html5lib')
-            a_tags = soup.find_all('a') # find all a tags
 
-            for a_tag in tqdm(a_tags):
-                url = a_tag.attrs.get('href', '')
-                if re.match(regex, url) is not None: # If regex matches (is a URL)
-                    if links.get(from_url) is None: # Create empty list for from_url (key)
-                        links.setdefault(from_url, []).append(url)
-                    else: # add to list for from_url (key)
-                        links[from_url].append(url)
+            # ---- Continue if string contains HTML elements ----
+            if re.match(regex_html, r.text) is not None: 
+                soup = bs4.BeautifulSoup(r.text, 'html5lib') # parse HTML
+                a_tags = soup.find_all('a') # find all a tags
+                
+                for a_tag in a_tags:
+                    url = a_tag.attrs.get('href', '')
+                    # if url > 0 and end with /, remove / from the last position
+                    url = url[:-1] if len(url) > 0 and url[-1] == '/' else url
 
-                    links = scrape_links(url, for_depth - 1, links) # RECURSIVE STEP
-        except:
-            return links # return 
+                    # ---- Continue if string contains URL pattern ----
+                    if re.match(regex_url, url) is not None: 
+                        epoch = datetime.datetime.now()
+
+                        links['found'] = links['found'] + 1
+                        elapsed = epoch - start
+
+                        # ---- print to console ----
+                        sys.stdout.write(f'\rnum_of_links: {links["found"]}, running_time (sec): {elapsed.total_seconds()}, for_depth: {for_depth}')
+                        sys.stdout.flush()
+
+                        if links['links'].get(from_url) is None: # Create empty list for from_url (key)
+                            links['links'].setdefault(from_url, []).append(url)
+                        else: # add to list for from_url (key)
+                            links['links'][from_url].append(url)
+                            
+                        links = scrape_links(url, for_depth - 1, links, start) # RECURSIVE STEP
+        # cancel crawler with ctrl-c
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        except Exception as e:
+            # build excetion dictionary to console print
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            e_dict = {message: e, exc_type: exc_type, fname: fname, lineno: exc_tb.tb_lineno}
+            print(f', exception: {e_dict}')
+            return links # return if request didn't returned ok
 
     return links
 
 
 if __name__ == '__main__':
-    print('')
-    parser = argparse.ArgumentParser()
+    try:
+        sys.stdout.write(f'')
+        parser = argparse.ArgumentParser()
 
-    parser.add_argument("url", help="Starting point for crawler")
-    parser.add_argument('-j', '--json', help='JSON dump')
-    parser.add_argument("-d", "--depth", help="Crawler Depth", type=int)
-    args = parser.parse_args()
+        parser.add_argument("url", help="Starting point for crawler")
+        parser.add_argument('-j', '--json', help='JSON dump')
+        parser.add_argument("-d", "--depth", help="Crawler Depth", type=int)
+        args = parser.parse_args()
 
-    url = args.url
-    file_name = args.json if args.json else None
-    depth = args.depth if args.depth else 0
+        url = args.url
+        file_name = args.json if args.json else None
+        depth = args.depth if args.depth else 0
 
-    scraped_links = scrape_links(url, depth)
+        scraped_links = scrape_links(url, depth)
 
-    if file_name is not None:
-        with open(f'{file_name}.json', 'w') as fp:
-            json.dump(scraped_links, fp)
-    else:
-        print(scraped_links)
-
-    if depth > 0:
-        print('\n') 
-    else:
+        if file_name is not None:
+            with open(f'{file_name}.json', 'w') as fp:
+                json.dump(scraped_links, fp)
+        else:
+            print(scraped_links)
         print('')
-
+        print('')
+    except KeyboardInterrupt:
+            print('')
+            print('')
+            print('catching crawler...')
+            sys.exit(0)
+    except:
+        print('')
+        print('')
+        sys.exit(1)
+    
